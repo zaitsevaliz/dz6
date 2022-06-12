71b0bef8d067798ab9fdebb41ad3f04bd7654ecb
zaitsevaliz <101466989+zaitsevaliz@users.noreply.github.com>
2022-06-12 21:08:11 +0300

    Initial commit

---
+++ js/FilterComponent.js
@@ -0,0 +1,8 @@
+Vue.component('search', {
+    template: `<form action="#" class="search-form" @submit.prevent='$parent.filter'>
+    <input type="text" class="search-field" v-model='$parent.userSearch'>
+    <button type="submit" class="btn-search">
+        <i class="fas fa-search"></i>
+    </button>
+</form>`
+})
